15b801c3e766bebf44bf58bbd439ffcfab7e2b09
danielemurgioni <dashomew@gmail.com>
2025-06-03 17:38:02 +0200

    [UPD] change reviews structure

--- src/pages/Moviepage.jsx
+++ src/pages/Moviepage.jsx
@@ -11,7 +11,27 @@ const initialMovies = [
         genre: "genere 1",
         release_year: "anno di rilascio 1",
         abstract: "La le lu lei lo La le lu lei lo 1",
-        image: "https://picsum.photos/500/300"
+        image: "https://picsum.photos/500/300",
+        reviews: [
+            {
+                id: 1,
+                text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur, molestiae eum, repellendus nostrum accusantium ducimus sed debitis",
+                author: "autore 1",
+                vote: 1
+            },
+            {
+                id: 2,
+                text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur, molestiae eum, repellendus nostrum accusantium ducimus sed debitis",
+                author: "autore 2",
+                vote: 2
+            },
+            {
+                id: 3,
+                text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur, molestiae eum, repellendus nostrum accusantium ducimus sed debitis",
+                author: "autore 3",
+                vote: 3
+            }
+        ]
     },
     {
         id: 2,
@@ -20,7 +40,27 @@ const initialMovies = [
         genre: "genere 2",
         release_year: "anno di rilascio 2",
         abstract: "La le lu lei lo La le lu lei lo 2",
-        image: "https://picsum.photos/500/300"
+        image: "https://picsum.photos/500/300",
+        reviews: [
+            {
+                id: 1,
+                text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur, molestiae eum, repellendus nostrum accusantium ducimus sed debitis",
+                author: "autore 1",
+                vote: 1
+            },
+            {
+                id: 2,
+                text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur, molestiae eum, repellendus nostrum accusantium ducimus sed debitis",
+                author: "autore 2",
+                vote: 2
+            },
+            {
+                id: 3,
+                text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur, molestiae eum, repellendus nostrum accusantium ducimus sed debitis",
+                author: "autore 3",
+                vote: 3
+            }
+        ]
     },
     {
         id: 3,
@@ -29,7 +69,27 @@ const initialMovies = [
         genre: "genere 3",
         release_year: "anno di rilascio 3",
         abstract: "La le lu lei lo La le lu lei lo 3",
-        image: "https://picsum.photos/500/300"
+        image: "https://picsum.photos/500/300",
+        reviews: [
+            {
+                id: 1,
+                text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur, molestiae eum, repellendus nostrum accusantium ducimus sed debitis",
+                author: "autore 1",
+                vote: 1
+            },
+            {
+                id: 2,
+                text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur, molestiae eum, repellendus nostrum accusantium ducimus sed debitis",
+                author: "autore 2",
+                vote: 2
+            },
+            {
+                id: 3,
+                text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur, molestiae eum, repellendus nostrum accusantium ducimus sed debitis",
+                author: "autore 3",
+                vote: 3
+            }
+        ]
     },
     {
         id: 4,
@@ -38,28 +98,27 @@ const initialMovies = [
         genre: "genere 4",
         release_year: "anno di rilascio 4",
         abstract: "La le lu lei lo La le lu lei lo 4",
-        image: "https://picsum.photos/500/300"
-    }
-]
-
-const initialReviews = [
-    {
-        id: 1,
-        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur, molestiae eum, repellendus nostrum accusantium ducimus sed debitis",
-        author: "autore 1",
-        vote: 1
-    },
-    {
-        id: 2,
-        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur, molestiae eum, repellendus nostrum accusantium ducimus sed debitis",
-        author: "autore 2",
-        vote: 2
-    },
-    {
-        id: 3,
-        text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur, molestiae eum, repellendus nostrum accusantium ducimus sed debitis",
-        author: "autore 3",
-        vote: 3
+        image: "https://picsum.photos/500/300",
+        reviews: [
+            {
+                id: 1,
+                text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur, molestiae eum, repellendus nostrum accusantium ducimus sed debitis",
+                author: "autore 1",
+                vote: 1
+            },
+            {
+                id: 2,
+                text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur, molestiae eum, repellendus nostrum accusantium ducimus sed debitis",
+                author: "autore 2",
+                vote: 2
+            },
+            {
+                id: 3,
+                text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur, molestiae eum, repellendus nostrum accusantium ducimus sed debitis",
+                author: "autore 3",
+                vote: 3
+            }
+        ]
     }
 ]
 
@@ -68,9 +127,8 @@ const Moviepage = () => {
     console.log(id)
 
     const [movies, setMovies] = useState(initialMovies)
-    const [reviews, setReviews] = useState(initialReviews)
 
-    const [movieDetail, setMovieDetail] = useState({})
+    const [movieDetail, setMovieDetail] = useState(null)
 
     //recupero l'oggetto della'array con l'id passato come parametro
     const fetchMovie = () => {
@@ -88,7 +146,7 @@ const Moviepage = () => {
 
     return (
         <>
-            <div className="row border border-3 border-success">
+            <div className="row gy-4 pb-4">
                 {/* effetuo un effetto di caricamento dei dati */}
                 {movieDetail === null ? (
                     `Caricamento dati Movie`
@@ -102,19 +160,16 @@ const Moviepage = () => {
                             <h3>{movieDetail.director}</h3>
                             <p>{movieDetail.abstract}</p>
                         </div>
+                        <div className="col-12">
+                            <div className="d-flex justify-content-between">
+                                <h3>Community Reviews</h3>
+                            </div>
+                        </div>
+                        {movieDetail.reviews.map((review) => (
+                            <ReviewCard review={review} key={`review-${review.id}`} />
+                        ))}
                     </>
                 )}
-            </div>
-            <div className="row mt-4 gy-4">
-                <div className="col-12">
-                    <div className="d-flex justify-content-between">
-                        <h3>Community Reviews</h3>
-                    </div>
-                </div>
-                {/* review */}
-                {reviews.map((review) => (
-                    <ReviewCard review={review} key={`review-${review.id}`} />
-                ))}
             </div>
         </>
     )
