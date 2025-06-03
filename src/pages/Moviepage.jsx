0b905ee87a3ce853e64921d58f3862fd32767cac
danielemurgioni <dashomew@gmail.com>
2025-06-03 15:41:06 +0200

    [UPD] styling Moviepage

--- src/pages/Moviepage.jsx
+++ src/pages/Moviepage.jsx
@@ -3,12 +3,13 @@ import React from 'react'
 const Moviepage = () => {
     return (
         <>
-            <div className="row">
+            <div className="row border border-3 border-success">
                 <div className="col-12 col-md-6 col-lg-4">
-                    Immagine
+                    <img className='img-fluid' src='https://picsum.photos/500/300' alt='movie' />
                 </div>
                 <div className="col-12 col-md-6 col-lg-8">
                     <h1>Titolo</h1>
+                    <h3>Attori</h3>
                     <p>Altre informazioni</p>
                 </div>
             </div>
