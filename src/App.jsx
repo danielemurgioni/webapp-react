1c1bc66c9693587e27573b2ca9631475099f2d71
danielemurgioni <dashomew@gmail.com>
2025-06-03 12:12:18 +0200

    [UPD] utilize react router dom components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import DefaultLayout from "./layouts/DefaultLayout"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
   return (
     <>
-      <DefaultLayout />
+      <BrowserRouter>
+        <Routes>
+          <Route Component={DefaultLayout}>
+            <Route index element={<h1>Homepage</h1>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
