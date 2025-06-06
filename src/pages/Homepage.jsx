f34cc5698a4e34220776b46758bb1b361196cb88
danielemurgioni <dashomew@gmail.com>
2025-06-06 17:45:48 +0200

    [UPD] render the fetched Movies

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,52 +2,13 @@ import axios from 'axios'
 import { useState, useEffect } from 'react'
 import MovieCard from '../components/MovieCard'
 
-const initialMovie = [
-    {
-        id: 1,
-        title: "titolo 1",
-        director: "direttore 1",
-        genre: "genere 1",
-        release_year: "anno di rilascio 1",
-        abstract: "La le lu lei lo La le lu lei lo 1",
-        image: "https://picsum.photos/500/300"
-    },
-    {
-        id: 2,
-        title: "titolo 2",
-        director: "direttore 2",
-        genre: "genere 2",
-        release_year: "anno di rilascio 2",
-        abstract: "La le lu lei lo La le lu lei lo 2",
-        image: "https://picsum.photos/500/300"
-    },
-    {
-        id: 3,
-        title: "titolo 3",
-        director: "direttore 3",
-        genre: "genere 3",
-        release_year: "anno di rilascio 3",
-        abstract: "La le lu lei lo La le lu lei lo 3",
-        image: "https://picsum.photos/500/300"
-    },
-    {
-        id: 4,
-        title: "titolo 4",
-        director: "direttore 4",
-        genre: "genere 4",
-        release_year: "anno di rilascio 4",
-        abstract: "La le lu lei lo La le lu lei lo 4",
-        image: "https://picsum.photos/500/300"
-    }
-]
-
 const Homepage = () => {
 
-    const [movies, setMovies] = useState(initialMovie)
+    const [movies, setMovies] = useState([])
 
     const fetchMovies = () => {
         axios.get("http://127.0.0.1:3000/api/movies").then((resp) => {
-            console.log(resp.data)
+            setMovies(resp.data)
         }).catch((err) => {
             console.log(err)
         })
